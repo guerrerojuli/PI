c96ef6408e0d4aea77d5e512bd7bb8b28fcae9a6
Julian Guerrero <julian1guerrero@gmail.com>
2024-05-18 09:56:33 -0300

    Taller 10

---
+++ 17-05/ej1.c
@@ -0,0 +1,39 @@
+#include "../utillist.h"
+#include <assert.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+TList concatenarLista(TList l1, const TList l2) {
+  if (l2 == NULL)
+    return l1;
+  if (l1 == NULL) {
+    TNode *node = malloc(sizeof(*node));
+    node->elem = l2->elem;
+    node->tail = concatenarLista(NULL, l2->tail);
+    return node;
+  }
+  l1->tail = concatenarLista(l1->tail, l2);
+  return l1;
+}
+
+int main(void) {
+  int v1[] = {4, 3, 7};
+  TList l1 = fromArray(v1, 3);
+  int v2[] = {3, 2, 6};
+  TList l2 = fromArray(v2, 3);
+  int v1yv2[] = {4, 3, 7, 3, 2, 6};
+  TList l1yl2 = concatenarLista(l1, l2);
+  assert(checkElems(l1yl2, v1yv2, 6));
+  freeList(l1yl2);
+  freeList(l2);
+
+  TList l3 = NULL;
+  TList l4 = fromArray(v1yv2, 6);
+  TList l3yl4 = concatenarLista(l3, l4);
+  assert(checkElems(l3yl4, v1yv2, 6));
+  freeList(l3yl4);
+  freeList(l4);
+
+  printf("OK!\n");
+  return 0;
+}
